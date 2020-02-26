b771fb8fd6cd3e483cac04773ec8ffead4790949
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-26 04:46:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,13 @@ class Student
     end 
     
   def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE grade = 9 
+    SQL
     
+    DB[:conn].execute(sql)
   end 
   
   def save
